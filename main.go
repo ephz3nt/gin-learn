dd33fefeaea545e6abbc5f10ad6ed209eec08f60
ephz3nt <root@painso.com>
2020-04-03 16:54:18 +0800

    gin with RESTful

--- main.go
+++ main.go
@@ -1,24 +1,44 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
-)
-
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	f, _ := ioutil.ReadFile("hello.html")
-	fmt.Fprintln(w, string(f))
-}
+import "github.com/gin-gonic/gin"
 
 func main() {
-	addr := "0.0.0.0:20000"
-	http.HandleFunc("/hello", sayHello)
-	err := http.ListenAndServe(addr, nil)
-	fmt.Println("server start on: ", addr)
-	if err != nil {
-		fmt.Println("server start failed, err: ", err)
-		return
-	}
+
+	// set mode
+
+	// gin.SetMode(gin.ReleaseMode)
+
+	// default route
+	r := gin.Default() // return default router
+
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"id":    1,
+			"name":  "张三",
+			"class": "清华",
+		})
+	})
+
+	r.GET("/book", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"method": "GET",
+		})
+	})
+	r.POST("/book", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"method": "POST",
+		})
+	})
+	r.PUT("/book", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"method": "PUT",
+		})
+	})
+	r.DELETE("/book", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"method": "DELETE",
+		})
+	})
+	r.Run(":20000")
 
 }
